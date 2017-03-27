8e385c7ec0e8e6957db683215f354851cef77dcf
Haeflinger <dhaeflinger@ntst.com>
2017-03-27 09:14:25 -0500

    Added some info

--- my_first_program.cs
+++ my_first_program.cs
@@ -1,4 +1,5 @@
 public static void main(string[]args){
 	console.writeLine("Hello World!");
+    Console.WriteLine("Obviously it's your first program. You don't even have a class and the naming is atrocious.");
 
 }
